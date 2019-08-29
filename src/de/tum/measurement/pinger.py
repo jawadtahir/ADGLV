9d4bc6359737cc53c778091023164ea75d071c39
Jawad Tahir <ranajawadtahir@gmail.com>
2019-08-29 05:51:50 +0200

    further logging for debug

--- src/de/tum/measurement/pinger.py
+++ src/de/tum/measurement/pinger.py
@@ -9,6 +9,9 @@ import os
 
 from de.tum.models import Measurement
 from de.tum.util.Constants import *
+from de.tum.util.utils import get_logger
+
+log = get_logger(__name__)
 
 
 def pinger(node_name, t_now, node_adrs, **kwargs):
@@ -16,6 +19,7 @@ def pinger(node_name, t_now, node_adrs, **kwargs):
     command = r"mtr --json --max-ttl 250 --tcp --port 22 --show-ips " + node_adrs
 #     command = r"mtr --json --max-ttl 250 --port 22 --show-ips " + node_adrs
     output = os.popen(command).read()
+    log.debug(output)
     mtr_reponse = json.loads(output)
 
     return Measurement(node_name, node_adrs, t_now, str(m_time), mtr_reponse, MTR_DELAY)
